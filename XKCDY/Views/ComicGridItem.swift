8761f7bf9018c7930529281a1067793b32448a1f
Max Isom <hi@maxisom.me>
2020-07-14 20:39:50 -0500

    Fade in comics

--- XKCDY/Views/ComicGridItem.swift
+++ XKCDY/Views/ComicGridItem.swift
@@ -34,32 +34,37 @@ struct ComicGridItem: View {
     var onTap: (Int) -> Void
     @EnvironmentObject var store: Store
     @State private var pulseEnded = false
+    @State private var isLoaded = false
 
     var body: some View {
         GeometryReader { geom -> AnyView in
             self.store.updatePosition(for: self.comic.id, at: CGRect(x: geom.frame(in: .global).midX, y: geom.frame(in: .global).midY, width: geom.size.width, height: geom.size.height))
 
-            let image = KFImage(self.comic.getBestImageURL()!)
-                .placeholder {
-                    Rectangle()
-                        .fill(Color.secondary)
-                        .opacity(self.pulseEnded ? 0.4 : 0.2)
-                        .frame(width: geom.size.width, height: geom.size.height)
-                        .animation(Animation.easeInOut(duration: 0.75).repeatForever())
-                        .onAppear {
-                            self.pulseEnded = true
-                    }
+            let stack = ZStack {
+                KFImage(self.comic.getBestImageURL()!, isLoaded: self.$isLoaded)
+                    .cancelOnDisappear(true)
+                .resizable()
+                .scaledToFill()
+                .opacity(self.isLoaded ? 1 : 0)
+
+                Rectangle()
+                    .fill(Color.secondary)
+                    .opacity(self.pulseEnded ? 0.4 : 0.2)
+                    .frame(width: geom.size.width, height: geom.size.height)
+                    .animation(Animation.easeInOut(duration: 0.75).repeatForever())
+                    .onAppear {
+                        self.pulseEnded = true
+                }
+                .opacity(self.isLoaded ? 0 : 1)
             }
-            .cancelOnDisappear(true)
-            .resizable()
-            .scaledToFill()
             .frame(width: geom.size.width, height: geom.size.height)
+            .animation(.easeIn(duration: 0.2))
             .onTapGesture {
                 self.onTap(self.comic.id)
             }
 
             return AnyView(
-                image
+                stack
                     .overlay(
                         ComicBadge(comic: self.comic).padding(EdgeInsets(top: 0, leading: 0, bottom: 5, trailing: 5)),
                         alignment: .bottomTrailing
