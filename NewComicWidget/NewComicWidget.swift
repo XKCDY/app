edcd7b5af571a6bd2d8849531ce07009ae140695
Max Isom <hi@maxisom.me>
2020-09-20 14:56:02 -0400

    Allow user config of widget

--- NewComicWidget/NewComicWidget.swift
+++ NewComicWidget/NewComicWidget.swift
@@ -12,9 +12,12 @@ import RealmSwift
 import class Kingfisher.KingfisherManager
 import protocol Kingfisher.Resource
 
-struct Provider: TimelineProvider {
+struct Provider: IntentTimelineProvider {
+    typealias Entry = LastComicEntry
+    typealias Intent = ViewLatestComicIntent
+
     func placeholder(in context: Context) -> LastComicEntry {
-        LastComicEntry(date: Date(), comic: .getTallSample(), uiImage: UIImage(named: "2329_2x.png")!, family: context.family)
+        LastComicEntry(date: Date(), comic: .getTallSample(), uiImage: UIImage(named: "2329_2x.png")!, family: context.family, shouldBlur: false)
     }
 
     func configureRealm() {
@@ -25,7 +28,7 @@ struct Provider: TimelineProvider {
         Realm.Configuration.defaultConfiguration.fileURL = realmFileURL
     }
 
-    func getEntry(family: WidgetFamily, completion: @escaping (LastComicEntry) -> Void) {
+    func getEntry(family: WidgetFamily, configuration: ViewLatestComicIntent, completion: @escaping (LastComicEntry) -> Void) {
         var comic: Comic = .getTallSample()
 
         self.configureRealm()
@@ -48,23 +51,23 @@ struct Provider: TimelineProvider {
                 return
             }
 
-            let entry = LastComicEntry(date: Date(), comic: comic, uiImage: image, family: family)
+            let entry = LastComicEntry(date: Date(), comic: comic, uiImage: image, family: family, shouldBlur: configuration.shouldBlurUnread == .some(1) && !comic.isRead)
 
             completion(entry)
         }
     }
 
-    func getSnapshot(in context: Context, completion: @escaping (LastComicEntry) -> Void) {
-        getEntry(family: context.family) {
+    func getSnapshot(for configuration: ViewLatestComicIntent, in context: Context, completion: @escaping (LastComicEntry) -> Void) {
+        getEntry(family: context.family, configuration: configuration) {
             completion($0)
         }
     }
 
-    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> Void) {
+    func getTimeline(for configuration: ViewLatestComicIntent, in context: Context, completion: @escaping (Timeline<LastComicEntry>) -> Void) {
         self.configureRealm()
 
         Store(isLive: false).partialRefetchComics { _ in
-            getEntry(family: context.family) { entry in
+            getEntry(family: context.family, configuration: configuration) { entry in
                 let refreshDate = Calendar.current.date(byAdding: .minute, value: 5, to: Date())!
 
                 let timeline = Timeline(entries: [entry], policy: .after(refreshDate))
@@ -80,6 +83,7 @@ struct LastComicEntry: TimelineEntry {
     let comic: Comic
     let uiImage: UIImage
     let family: WidgetFamily
+    let shouldBlur: Bool
 
     var relevance: TimelineEntryRelevance? {
         TimelineEntryRelevance(score: comic.isRead ? 0 : 70)
@@ -119,7 +123,7 @@ struct NewComicWidgetEntryView: View {
     var body: some View {
         ZStack {
             GeometryReader { geom in
-                Image(uiImage: entry.uiImage).resizable().aspectRatio(contentMode: .fill) //.frame(width: geom.size.width, height: geom.size.height)
+                Image(uiImage: entry.uiImage).resizable().aspectRatio(contentMode: .fill).blur(radius: entry.shouldBlur ? 2 : 0)
 
                 Rectangle().fill(LinearGradient(gradient: Gradient(colors: [Color.black.opacity(0.8), Color.black.opacity(0.3), Color.white.opacity(0)]), startPoint: .top, endPoint: .bottom)).frame(width: geom.size.width, height: geom.size.height / 2)
 
@@ -142,9 +146,9 @@ struct NewSmallComicWidgetEntryView: View {
         VStack(alignment: .leading) {
             ComicBadgeHeader(comic: entry.comic)
 
-            Text(entry.comic.title).font(.headline).lineLimit(1).padding(.bottom, 1)
+            Text(verbatim: entry.comic.title).font(.headline).lineLimit(1).padding(.bottom, 1)
 
-            Text(entry.comic.alt).font(.caption)
+            Text(verbatim: entry.comic.alt).font(.caption)
         }.padding()
         .widgetURL(URL(string: "xkcdy://comics/\(entry.comic.id)"))
     }
@@ -155,7 +159,7 @@ struct NewComicWidget: Widget {
     let kind: String = "NewComicWidget"
 
     var body: some WidgetConfiguration {
-        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+        IntentConfiguration(kind: kind, intent: ViewLatestComicIntent.self, provider: Provider()) { entry in
             Group {
                 if entry.family == .systemSmall {
                     NewSmallComicWidgetEntryView(entry: entry)
@@ -172,13 +176,16 @@ struct NewComicWidget: Widget {
 struct NewComicWidget_Previews: PreviewProvider {
     static var previews: some View {
         Group {
-            NewSmallComicWidgetEntryView(entry: LastComicEntry(date: Date(), comic: .getTallSample(), uiImage: UIImage(named: "2329_2x.png")!, family: .systemSmall))
+            NewSmallComicWidgetEntryView(entry: LastComicEntry(date: Date(), comic: .getTallSample(), uiImage: UIImage(named: "2329_2x.png")!, family: .systemSmall, shouldBlur: false))
                 .previewContext(WidgetPreviewContext(family: .systemSmall))
 
-            NewComicWidgetEntryView(entry: LastComicEntry(date: Date(), comic: .getTallSample(), uiImage: UIImage(named: "2329_2x.png")!, family: .systemMedium))
+            NewComicWidgetEntryView(entry: LastComicEntry(date: Date(), comic: .getTallSample(), uiImage: UIImage(named: "2329_2x.png")!, family: .systemMedium, shouldBlur: false))
                 .previewContext(WidgetPreviewContext(family: .systemMedium))
 
-            NewComicWidgetEntryView(entry: LastComicEntry(date: Date(), comic: .getTallSample(), uiImage: UIImage(named: "2329_2x.png")!, family: .systemLarge))
+            NewComicWidgetEntryView(entry: LastComicEntry(date: Date(), comic: .getTallSample(), uiImage: UIImage(named: "2329_2x.png")!, family: .systemMedium, shouldBlur: true))
+                .previewContext(WidgetPreviewContext(family: .systemMedium))
+
+            NewComicWidgetEntryView(entry: LastComicEntry(date: Date(), comic: .getTallSample(), uiImage: UIImage(named: "2329_2x.png")!, family: .systemLarge, shouldBlur: false))
                 .previewContext(WidgetPreviewContext(family: .systemLarge))
         }
     }
