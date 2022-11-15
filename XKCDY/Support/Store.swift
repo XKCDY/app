4a129121fd563aa6a8cc2dc3793e9a9a8482bbd0
Max Isom <hi@maxisom.me>
2022-11-14 20:16:35 -0800

    Don't need frozen observer

--- XKCDY/Support/Store.swift
+++ XKCDY/Support/Store.swift
@@ -55,12 +55,10 @@ final class Store: ObservableObject {
     @Published var currentFavoriteIds: [Int] = []
     @Published var filteredComics: Results<Comic> {
         didSet {
-            self.addFrozenObserver()
             self.cacheNextShuffleResult()
         }
     }
     @Published var timeComicFrames: AnyRealmCollection<TimeComicFrame>
-    @Published var frozenFilteredComics: Results<Comic>
 
     @ObservedObject private var userSettings = UserSettings()
     @ObservedObject private var comics: RealmSwift.List<Comic>
@@ -86,12 +84,9 @@ final class Store: ObservableObject {
         let initialComics = comics!.comics.sorted(byKeyPath: "id", ascending: false)
         self.filteredComics = initialComics
 
-        self.frozenFilteredComics = initialComics.freeze().sorted(byKeyPath: "id", ascending: false)
-
         self.timeComicFrames = AnyRealmCollection(realm.objects(TimeComicFrame.self).sorted(byKeyPath: "frameNumber", ascending: true))
 
         self.updateFilteredComics()
-        self.addFrozenObserver()
         self.cacheNextShuffleResult()
 
         if self.isLive {
@@ -119,14 +114,6 @@ final class Store: ObservableObject {
         }
 
         return filtered!
-    }
-
-    private func addFrozenObserver() {
-        if self.isLive {
-            self.comicsToken = self.filteredComics.observe { _ in
-                self.frozenFilteredComics = self.filteredComics.freeze().sorted(byKeyPath: "id", ascending: false)
-            }
-        }
     }
 
     private func updateFilteredComics() {
