feecb59ccb8ce896e5c72f30a7d6c1c6d108a6a4
Max Isom <hi@maxisom.me>
2022-11-16 21:20:17 -0800

    Cleanup

--- XKCDY/Views/ComicsGrid.swift
+++ XKCDY/Views/ComicsGrid.swift
@@ -10,45 +10,10 @@ import SwiftUI
 import UIKit
 import Combine
 import RealmSwift
-import SwiftUILayouts
-import class Kingfisher.ImagePrefetcher
-import class Kingfisher.ImageCache
 
 enum ScrollDirection {
     case up, down
 }
-
-//class WaterfallScreenLayoutDelegate: ASCollectionViewDelegate, ASWaterfallLayoutDelegate {
-//    public var collectionView: Binding<UICollectionView?> = .constant(nil)
-//
-//    func heightForHeader(sectionIndex: Int) -> CGFloat? {
-//        0
-//    }
-//
-//    func heightForCell(at indexPath: IndexPath, context: ASWaterfallLayout.CellLayoutContext) -> CGFloat {
-//        guard let comic: Comic = getDataForItem(at: indexPath) else { return 100 }
-//        let height = context.width / CGFloat(comic.imgs!.x1!.ratio)
-//        return height
-//    }
-//
-//    func collectionView(_ collectionView: UICollectionView, targetContentOffsetForProposedContentOffset proposedContentOffset: CGPoint) -> CGPoint {
-//        self.collectionView.wrappedValue = collectionView
-//
-//        return proposedContentOffset
-//    }
-//}
-//
-//extension ASWaterfallLayout.ColumnCount: Equatable {
-//    public static func == (lhs: ASWaterfallLayout.ColumnCount, rhs: ASWaterfallLayout.ColumnCount) -> Bool {
-//        switch (lhs, rhs) {
-//        case (.fixed(let a), .fixed(let b)):
-//            return a == b
-//        default:
-//            return false
-//        }
-//
-//    }
-//}
 
 @propertyWrapper struct PublishedWithoutViewUpdate<Value> {
     let publisher: PassthroughSubject<Void, Never>
@@ -99,12 +64,6 @@ class ScrollStateModel: ObservableObject {
     }
 }
 
-struct CustomCell: View {
-    var body: some View {
-        Text("hi")
-    }
-}
-
 struct ComicsGridView: View {
     @State var columnMinSize: CGFloat = 150
     @State var inViewUrls: [String] = []
@@ -118,18 +77,7 @@ struct ComicsGridView: View {
     @State private var lastScrollPositions: [CGFloat] = []
     @State private var shouldBlurStatusBar = false
     @ObservedObject private var scrollState = ScrollStateModel()
-
-//    func onCellEvent(_ event: CellEvent<Comic>) {
-//        switch event {
-//        case let .prefetchForData(data):
-//            ImagePrefetcher(urls: data.map {$0.getReasonableImageURL()!}).start()
-//        case let .cancelPrefetchForData(data):
-//            ImagePrefetcher(urls: data.map {$0.getReasonableImageURL()!}).stop()
-//        default:
-//            return
-//        }
-//    }
-
+    
     func handleComicTap(of comicId: Int) {
         self.store.currentComicId = comicId
         self.onComicOpen()
